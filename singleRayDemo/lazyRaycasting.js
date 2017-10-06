3a160dcfc3194dc20a2f82298f8beca2a79026ba
Roland Rytz <roland.rytz@gmail.com>
2017-10-06 21:17:22 +0200

    Implement some basic demos and a first proper volume ray caster

---
+++ singleRayDemo/lazyRaycasting.js
@@ -0,0 +1,109 @@
+
+
+var img = document.getElementById("slice");
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+canvas.height = img.height * Math.sqrt(2);
+canvas.width = img.width * Math.sqrt(2);
+
+var cutCanvas = document.getElementById("cut");
+var cutContext = cutCanvas.getContext("2d");
+
+cutCanvas.height = 1;
+cutCanvas.width = img.width * Math.sqrt(2);
+
+context.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2);
+var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+var cutImageData = cutContext.getImageData(0, 0, cutCanvas.width, cutCanvas.height);
+
+var angle = 0;
+var rayLength = img.width * Math.sqrt(2);
+var samplingRate = 1;
+var sampleCount = samplingRate*rayLength;
+var sampleDistance = rayLength/sampleCount;
+
+context.fillStyle = "#00FF00";
+context.strokeStyle = "#FF0000";
+
+console.log(imageData);
+
+draw();
+
+
+function draw(){
+
+	var xPos = rayLength/2;
+
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2);
+
+	var pos = [
+		Math.sin(angle)*rayLength/2 + Math.cos(angle)*((xPos-rayLength/2)/rayLength)*rayLength + rayLength/2,
+		Math.cos(angle)*rayLength/2 - Math.sin(angle)*((xPos-rayLength/2)/rayLength)*rayLength + rayLength/2
+	];
+	var increment = [Math.sin(angle)*sampleDistance, Math.cos(angle)*sampleDistance];
+
+	context.beginPath();
+	context.moveTo(
+		Math.sin(angle)*rayLength/2-Math.cos(angle)*rayLength/2 + rayLength/2,
+		Math.cos(angle)*rayLength/2+Math.sin(angle)*rayLength/2 + rayLength/2
+	);
+	context.lineTo(
+		Math.sin(angle)*rayLength/2+Math.cos(angle)*rayLength/2 + rayLength/2,
+		Math.cos(angle)*rayLength/2-Math.sin(angle)*rayLength/2 + rayLength/2
+	);
+	context.stroke();
+	
+	for(var i = 0; i < sampleCount; i++){
+
+		var value = getColorAtCoord(Math.round(pos[0]), Math.round(pos[1]), imageData);
+		cutImageData.data[i*4+0] = value[0];
+		cutImageData.data[i*4+1] = value[1];
+		cutImageData.data[i*4+2] = value[2];
+		cutImageData.data[i*4+3] = value[3];
+		
+		context.fillRect(Math.round(pos[0]), Math.round(pos[1]), 1, 1);
+		pos[0] -= increment[0];
+		pos[1] -= increment[1];
+	}
+	cutContext.putImageData(cutImageData, 0, 0);
+
+	angle += 0.01;
+	setTimeout(draw, 16);
+
+}
+
+function getColorAtCoord(x, y, imageData){
+
+	if(x<0 || x>imageData.width || y<0 || y>imageData.height){
+		return [0, 0, 0, 0];
+	}
+
+	var pos = (x + y*imageData.width)*4;
+	return [
+		 imageData.data[pos+0]
+		,imageData.data[pos+1]
+		,imageData.data[pos+2]
+		,imageData.data[pos+3]
+	];
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
