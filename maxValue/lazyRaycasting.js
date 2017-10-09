9dfe1366cf4d672c52a0e4b4d1316a167d51e090
Roland Rytz <roland.rytz@gmail.com>
2017-10-09 16:12:39 +0200

    Update maximum intensity projection

--- maxValue/lazyRaycasting.js
+++ maxValue/lazyRaycasting.js
@@ -3,7 +3,7 @@
 var img = document.getElementById("slice");
 
 img.onload = render;
-img.src = "./water.png";
+img.src = "../vessels.png";
 
 function render(){
 	var canvas = document.getElementById("canvas");
@@ -25,47 +25,71 @@ function render(){
 
 	var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
+	var imageSize = imageData.width;
+	var volumeData = new Uint8Array(imageData.data.length/4);
+	for(var i = 0; i < volumeData.length; i++){
+		volumeData[i] = imageData.data[i*4];
+	}
+
 	var cutImageData = cutContext.getImageData(0, 0, cutCanvas.width, cutCanvas.height);
 
-	var angle = 1;
+	var startAngle = 0;
+	var startTime = Date.now();
+	var turnsPerSecond = 0.1;
+	
 	var rayLength = img.width * Math.sqrt(2);
-	var samplingRate = 1;
+	var samplingRate = 0.5;
 	var sampleCount = samplingRate*rayLength;
 	var sampleDistance = rayLength/sampleCount;
-
-	console.log(imageData);
 
 	draw();
 
 
 	function draw(){
+
+		var angle = ((Date.now()-startTime)/1000)*(2*Math.PI*turnsPerSecond);
 		
 		var increment = [Math.sin(angle)*sampleDistance, Math.cos(angle)*sampleDistance];
+		var baseX = Math.sin(angle)*rayLength/2 + img.width/2;
+		var baseY = Math.cos(angle)*rayLength/2 + img.width/2;
+		var cosAngle = Math.cos(angle);
+		var sinAngle = Math.sin(angle);
 
 		for(var z = 0; z < zSize; z++){
-			for(var s = 0; s < sampleCount; s++){
-				var xPos = s;
+			var imageZPos = z*cutImageData.width*4;
+			for(var s = 0; s < rayLength; s++){
 
 				var pos = [
-					Math.sin(angle)*rayLength/2 + Math.cos(angle)*((xPos-rayLength/2)/rayLength)*rayLength + img.width/2,
-					Math.cos(angle)*rayLength/2 - Math.sin(angle)*((xPos-rayLength/2)/rayLength)*rayLength + img.width/2
+					baseX + cosAngle*((s-rayLength/2)/rayLength)*rayLength,
+					baseY - sinAngle*((s-rayLength/2)/rayLength)*rayLength
 				];
 
-				var max = 0;
+				var output = 0;
+
+				var x = 0;
+				var y = 0;
+				var value = 0;
+				var v = 0;
 				
 				for(var i = 0; i < sampleCount; i++){
+					
+					x = Math.floor(pos[0]);
+					y = Math.floor(pos[1]*1.3-25);
+					value = 0;
 
-					//context.fillRect(Math.round(pos[0]), Math.round(pos[1]), 1, 1);
-					var value = getColorAtCoord(Math.round(pos[0]), Math.round(pos[1]), z, imageData);
-					max = Math.max(max, value[0]);
+					if(!(x<0 || x>=imageSize || y<0 || y>=imageSize)){
+						value = volumeData[x + z*imageSize + y*imageSize*imageSize];
+					}
+					
+					output = Math.max(value, output);
 					pos[0] -= increment[0];
 					pos[1] -= increment[1];
 				}
 
-				cutImageData.data[z*cutImageData.width*4 + s*4+0] = max;
-				cutImageData.data[z*cutImageData.width*4 + s*4+1] = max;
-				cutImageData.data[z*cutImageData.width*4 + s*4+2] = max;
-				cutImageData.data[z*cutImageData.width*4 + s*4+3] = 255;
+				cutImageData.data[imageZPos + s*4+0] = output;
+				cutImageData.data[imageZPos + s*4+1] = output;
+				cutImageData.data[imageZPos + s*4+2] = output;
+				cutImageData.data[imageZPos + s*4+3] = 255;
 
 			}
 
@@ -74,26 +98,31 @@ function render(){
 		
 		cutContext.putImageData(cutImageData, 0, 0);
 
-		angle += 0.1;
-		setTimeout(draw, 16);
+		frameCount++;
+		
+		requestAnimationFrame(draw);
 
 	}
 
-	function getColorAtCoord(x, y, z, imageData){
+	var samplesPerFrame = Math.round(zSize*rayLength*sampleCount);
+	document.getElementById("samplesPerFrame").innerHTML = samplesPerFrame.toLocaleString();
 
-		if(x<0 || x>=imageData.width || y<0 || y>=imageData.width){
-			return [0, 0, 0, 0];
-		}
+	var frameCount = 0;
+	var lastFPS = Date.now();
+	setTimeout(updateFPS, 1000);
 
-		var pos = (x + y*imageData.width + z*imageData.width*imageData.width)*4;
-		return [
-			 imageData.data[pos+0]
-			,imageData.data[pos+1]
-			,imageData.data[pos+2]
-			,imageData.data[pos+3]
-		];
+	function updateFPS(){
+
+		var timeDifference = Date.now()-lastFPS;
+		lastFPS = Date.now();
+		var fps = frameCount/(timeDifference/1000);
+		frameCount = 0;
+		document.getElementById("framesPerSecond").innerHTML = Math.round(fps);
+		document.getElementById("samplesPerSecond").innerHTML = Math.round(samplesPerFrame*fps).toLocaleString();
+		setTimeout(updateFPS, 1000);
 		
 	}
+	
 }
 
 
